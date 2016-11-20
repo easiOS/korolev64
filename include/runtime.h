bb8905cf28f7fd9872f883bfa55d223480fd46e4
easimer <easimer@gmail.com>
2016-11-20 20:54:14 +0100

    Document KOM executables

--- include/runtime.h
+++ include/runtime.h
@@ -1,6 +1,15 @@
 #ifndef H_RUNTIME
 #define H_RUNTIME
 
+#define KOM_DEST 0x3D09000
+
+/*
+KOM executables are like COM files from DOS, but this has a minimal header. KOM
+files are loaded at 0x3D09000.
+It doesn't support relocation or any fancy things like that, but the programmer
+can define a entry point. The header is defined below (kom_hdr_t).
+*/
+
 typedef struct {
 	BYTE signature[4]; // KOM0
 	LONG entry_point;
