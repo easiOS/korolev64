e823d8955a652e4ba0b0c7f9d9d6621de4eb072c
easimer <easimer@gmail.com>
2016-11-29 19:09:20 +0100

    Change KOM load address to 0x3C00000

--- include/runtime.h
+++ include/runtime.h
@@ -1,11 +1,11 @@
 #ifndef H_RUNTIME
 #define H_RUNTIME
 
-#define KOM_DEST 0x3D09000
+#define KOM_DEST 0x3C00000
 
 /*
 KOM executables are like COM files from DOS, but this has a minimal header. KOM
-files are loaded at 0x3D09000.
+files are loaded at 0x3C00000.
 It doesn't support relocation or any fancy things like that, but the programmer
 can define a entry point. The header is defined below (kom_hdr_t).
 */
