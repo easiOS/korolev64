405055ee8c70c686bc5d3de6353726ce89a35c53
easimer <easimer@gmail.com>
2016-12-17 11:01:43 +0100

    [stdlibc] Add definition of strncmp, create errno.h

---
+++ include/errno.h
@@ -0,0 +1,6 @@
+#define EPERM		1
+#define ENOENT		2
+#define ENOEXEC		8
+#define EEXIST		17
+#define EMFILE		24
+#define ENOSPC		28
--- include/string.h
+++ include/string.h
@@ -9,6 +9,7 @@ void* kmemset(void* dest, BYTE val, LONG count);
 unsigned strlen(char* s);
 void reverse(char* s);
 unsigned strlen(char* s);
+int strncmp( const char *ptr0, const char *ptr1, int len );
 
 #endif /* __linux__ */
 
